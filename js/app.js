6c23398cdb073adad213143a78b405579261d603
ilana <ilanasuied@gmail.com>
2024-02-05 15:08:39 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//const variables
+const game1 = document.getElementById("first_game")
+const initialSrc = game1.src;
+
+//execute this function when th mouse is over
+function onOverImage(){
+    game1.src = "../images/giphy.gif"
+
+}
+
+//execute this functioin when the mouse is out
+function onoutImage(){
+    game1.src = initialSrc;
+}
+
+game1.addEventListener("mouseover", onOverImage)
+game1.addEventListener("mouseout", onoutImage)
