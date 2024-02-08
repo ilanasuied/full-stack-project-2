836a597b2db9b3f6d9e82ed28a1ef2b1ce4b77a9
ilana <ilanasuied@gmail.com>
2024-02-08 20:49:05 +0200

    add the game2 to index

--- js/app.js
+++ js/app.js
@@ -5,54 +5,45 @@ window.addEventListener('load', function(event){
 })
 
 
-//const variables for the first game
-const game1 = document.getElementById("first_game")
-const initialSrc = game1.src;
-const initialBorder = game1.style.border;
-
-
-//const variables for the second game
-const game2 = document.getElementById("second_game")
-const initialSrc2 = game2.src;
-const initialBorder2 = game2.style.border;
-
-
-//execute this function when the mouse is over
-function onOverImage() {
-    game1.src = "./images/giphy (2).gif";
-    game1.style.border = "5px solid blue";
-    game1.style.transform = 'scale(1.2)';
+//show the gif when the cursor is over the img
+function mouseoverImage(imageId, gifPath){
+    let game = document.getElementById(imageId);
+    game.src = gifPath;
+    game.style.border = "5px solid blue";
+    game.style.transform = 'scale(1.2)';
 }
 
-//execute this functioin when the mouse is out
-function onoutImage() {
-    game1.src = initialSrc;
-    game1.style.border = initialBorder;
-    game1.style.transform = 'scale(1)';
+//when the mouse is out on the image, show the inital image 
+function mouseoutImage(imageId, imgPath){
+    let game = document.getElementById(imageId);
+    game.src = imgPath;
+    game.style.border = "5px solid rgba(33, 34, 51, 0.9)";
+    game.style.transform = 'scale(1.0)';
 }
 
-//call the function when the mouse is over/out the image
-game1.addEventListener("mouseover", onOverImage)
-game1.addEventListener("mouseout", onoutImage)
 
+//call the function when the mouse is over/out the images
+document.getElementById("space").addEventListener("mouseover", function(){
+    mouseoverImage('space', './images/space.gif');
+})
+document.getElementById("space").addEventListener("mouseout", function(){
+    mouseoutImage('space', './images/space-624054_1280.jpg')
+})
 
-//execute this function when the mouse is over
-function onOverImage2() {
-    game2.src = "./images/giphy (3).gif";
-    game2.style.border = "5px solid blue";
-    game2.style.transform = 'scale(1.2)';
-}
+document.getElementById("first_game").addEventListener("mouseover", function(){
+    mouseoverImage('first_game', "./images/giphy (2).gif");
+});
+document.getElementById("first_game").addEventListener("mouseout", function(){
+    mouseoutImage('first_game', "./images/istockphoto-1424506235-1024x1024.jpg" );
+});
 
-//execute this functioin when the mouse is out
-function onoutImage2() {
-    game2.src = initialSrc2;
-    game2.style.border = initialBorder2;
-    game2.style.transform = 'scale(1)';
-}
+document.getElementById("second_game").addEventListener("mouseover", function(){
+    mouseoverImage('second_game', './images/giphy (3).gif');
+});
+document.getElementById("second_game").addEventListener("mouseout", function(){
+    mouseoutImage('second_game', './images/istockphoto-1204787034-1024x1024.jpg');
+});
 
-//call the function when the mouse is over/out the image
-game2.addEventListener("mouseover", onOverImage2)
-game2.addEventListener("mouseout", onoutImage2)
 
 
 
